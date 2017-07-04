82e8349be789aa325298343fb52d53402badc5b2
Benjamin Lei <benlei.cse@gmail.com>
2017-07-03 22:18:11 -0700

    use an actual logger

--- src/main/java/com/github/poeatlas/cli/Main.java
+++ src/main/java/com/github/poeatlas/cli/Main.java
@@ -2,21 +2,36 @@ package com.github.poeatlas.cli;
 
 import static java.lang.System.out;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.github.poeatlas.cli.ggpk.GgpkReader;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 public class Main {
-  public static void main(final String[] args) throws IOException {
-    System.exit(new Main().processGgpk(args));
+  /**
+   * Processes GGPK files.
+   */
+  public static void main(final String[] args) {
+    final Main main = new Main();
+
+    try {
+      System.exit(main.processGgpk(args));
+    } catch (Throwable th) {
+      log.error("Failed to complete extraction of GGPK file.");
+      log.debug(th.getMessage(), th);
+    }
   }
 
   private int processGgpk(final String[] args) throws IOException {
-    final OptionParser parser = new OptionParser();
+    final OptionParser parser = new OptionParser("vv");
 
     final OptionSpec<File> input = parser.accepts("input", "The GGPK inputFile.")
         .withRequiredArg()
@@ -27,7 +42,18 @@ public class Main {
         .withRequiredArg()
         .ofType(File.class);
 
+    final OptionSpec<Level> verbose = parser.accepts("verbose", "Level of "
+                                                                 + "verbosity to output.")
+        .withRequiredArg()
+        .ofType(Level.class);
+
     final OptionSet opt = parser.parse(args);
+
+    // sets the verbosity of output for everything
+    if (opt.has(verbose)) {
+      Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+      root.setLevel(opt.valueOf(verbose));
+    }
 
     if (!opt.has(input) || !opt.has(output)) {
       parser.printHelpOn(out);
