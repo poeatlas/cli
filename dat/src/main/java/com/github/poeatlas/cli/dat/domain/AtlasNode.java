3c965a22a9e8e8fcdbc6b6fbb90c76334936bd12
NothingSoup <linglijs@gmail.com>
2017-07-29 16:10:43 -0700

    fixed issue with 0'th PK not generating in table for AtlasNode

--- dat/src/main/java/com/github/poeatlas/cli/dat/domain/AtlasNode.java
+++ dat/src/main/java/com/github/poeatlas/cli/dat/domain/AtlasNode.java
@@ -9,7 +9,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -28,7 +27,6 @@ import javax.persistence.Transient;
 @Table(name = "atlas_node")
 public class AtlasNode {
   @Id
-  @GeneratedValue
   private int id;
 
   @Column(nullable = false)
